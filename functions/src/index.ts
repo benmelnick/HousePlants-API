37a7ad4b956b3d8fae46e531d7596894d477d91a
benmelnick <bmelnick3@gatech.edu>
2021-02-03 10:50:06 -0500

    Protect CRUD API operations with user ID token validation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as express from "express";
@@ -11,6 +13,12 @@ admin.initializeApp(functions.config().firebase);
 const app = express();
 const main = express();
 
+// initialize DB and name of user collection in firestore
+const db = admin.firestore();
+const userCollection = "users";
+
+const logger = functions.logger;
+
 // add path and middleware for handling requests
 main.use("/api/v1", app);
 
@@ -18,14 +26,39 @@ main.use("/api/v1", app);
 main.use(bodyParser.json());
 main.use(bodyParser.urlencoded({extended: false}));
 
-// initialize DB and name of user collection in firestore
-const db = admin.firestore();
-const userCollection = "users";
+// Express middleware function to validate Firebase ID tokens passed in
+// Authorization HTTP header. Token must be passed as a Bearer token:
+//   'Authorization: Bearer <Token>'
+const validateFirebaseIdToken = async (req: any, res: any, next: any) => {
+  logger.log("Checking if request is authorized with Firebase ID token");
 
-const logger = functions.logger;
+  // check to see if header contains 'Authorization' field
+  if (!req.headers.authorization || !req.headers.authorization.startsWith("Bearer ")) {
+    logger.error("No Firebase ID token was passed as a Bearer token in the Authorization header.",
+      "Make sure you authorize your request by providing the following HTTP header:",
+      "Authorization: Bearer <Firebase ID Token>");
+    res.status(403).send("Unauthorized");
+    return;
+  }
 
-// define function name, assign express server to handle HTTPS requests
-export const webApi = functions.https.onRequest(main);
+  // validate the token passed in and see if it corresponds to an actual user
+  logger.log("Found 'Authorization' header");
+  let idToken = req.headers.authorization.split("Bearer ")[1];
+
+  try {
+    const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+    logger.log("ID token correctly decoded: ", decodedIdToken);
+    req.user = decodedIdToken;
+    next();
+  } catch (error) {
+    console.error("Error while verifying Firebase ID token: ", error);
+    res.status(403).send("Unauthorized");
+  }
+};
+
+// all requests to endpoints in main server must pass token check
+// thus all HTTPS endpoints can only be accessed by Firebase users
+app.use(validateFirebaseIdToken);
 
 // users collection model
 interface User {
@@ -57,6 +90,7 @@ app.post("/users", async (req, res) => {
 // get all users
 // GET /users
 app.get("/users", async (req, res) => {
+  logger.log("Fetching all users from Firestore");
   try {
     const userQuerySnapshot = await db.collection(userCollection).get();
     const users: any[] = [];
@@ -74,3 +108,8 @@ app.get("/users", async (req, res) => {
     res.status(500).send(error);
   }
 });
+
+// creation function for API, assign express server to handle HTTPS requests
+// makes CRUD API in app available at:
+//  'https://<region>-house-plants-api.cloudfunctions.net/webApi
+export const webApi = functions.https.onRequest(main);
